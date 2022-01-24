f61c8bf84f81bc9d09af5e3a6171c9b9ec1cddec
alexthegreat194 <alexander.gentry@aol.com>
2022-01-24 15:35:12 -0800

    finished assingment

--- songs_albums.sql
+++ songs_albums.sql
@@ -6,6 +6,13 @@
  * 
  * Note that album - song is a one-to-many relationship, so no bridge table is needed.
  */
+ 
+CREATE TABLE Songs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(70) NOT NULL,
+    album_id INTEGER NOT NULL,
+    FOREIGN KEY(album_id) REFERENCES Albums(id)
+);
 
 CREATE TABLE Albums (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -18,14 +25,24 @@ CREATE TABLE Albums (
  * TODO: Insert at least 4 rows of data into the songs table. You can change up the albums as well. :)
  */
  
-INSERT INTO Albums
+ INSERT INTO Albums
     (name, artist, year_published)
-VALUES
+ VALUES
     ('The Dark Side of the Moon', 'Pink Floyd', 1973),
     ('Abbey Road', 'The Beatles', 1969),
     ('Hotel California', 'Eagles', 1976),
     ('Born in the U.S.A.', 'Bruce Springsteen', 1984),
     ('California', 'Blink-182', 2016)
+;
+ 
+INSERT INTO Songs
+    (name, album_id)
+VALUES
+    ('funny', 1),
+    ('gangnum style', 1),
+    ('Levitating', 3),
+    ('Kiss Me More', 4),
+    ('California things', 4)
 ;
 
 /* Required to get result in column format */
@@ -42,16 +59,28 @@ SELECT * FROM Albums;
 /* 
  * TODO: Write a table join query to construct a table of Song Name : Album Name
  */
+SELECT Songs.name, Albums.name FROM Songs
+JOIN Albums ON Songs.album_id = Albums.id;
 
 /*
  * TODO: Find all albums published between 1970 and 1980.
  */
+SELECT * FROM Albums
+WHERE year_published >= 1970 
+AND year_published <= 1980;
 
 /*
  * TODO: Find all songs on albums published between 1970 and 1980. 
  *(Hint: Use a table join.)
  */
+SELECT * FROM Songs
+JOIN Albums ON Songs.album_id = Albums.id
+WHERE year_published >= 1970 
+AND year_published <= 1980;
+
  
 /*
  * TODO: Find all songs on albums with names containing 'California'.
  */
+SELECT * FROM Songs
+WHERE name LIKE "%california%"
